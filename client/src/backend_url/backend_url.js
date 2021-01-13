8303b58a83141575b632c55d2bd315f459337768
bhr94 <bahar.mubtadayin@gmail.com>
2021-01-13 14:35:24 -0800

    changed the code in bookshelfjs file

--- client/src/backend_url/backend_url.js
+++ client/src/backend_url/backend_url.js
@@ -1,3 +1,3 @@
-// const backend_url = "http://localhost:8080";
-const backend_url = "https://evening-gorge-97906.herokuapp.com"
+const backend_url = "http://localhost:8080";
+// const backend_url = "https://evening-gorge-97906.herokuapp.com"
 export default backend_url;
--- server/knexfile.js
+++ server/knexfile.js
@@ -1,16 +1,24 @@
 module.exports = {
-  client: "mysql",
-  // connection: {
-  //   host: "127.0.0.1",
-  //   user: "root",
-  //   password: "root",
-  //   database: "capstone",
-  //   charset: "utf8",
+  // development: {
+    client: "mysql",
+    connection: {
+      host: "127.0.0.1",
+      user: "root",
+      password: "root",
+        database: "capstone",
+        charset: "utf8",
+    },
+    production: {
+      client: 'mysql',
+      connection: process.env.JAWSDB_URL,
+    }
+  }
+
+  // production: {
+  //   host: "td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  //   user: "h9p1f0m1ufohw6c8",
+  //   password: "bvjuneizhu024ct8",
+  //   connection:
+  //     "mysql://h9p1f0m1ufohw6c8:bvjuneizhu024ct8@td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/zuyd4r66g2sp77rz",
   // },
-  production: {
-    host: "td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "h9p1f0m1ufohw6c8",
-    password: "bvjuneizhu024ct8",
-    connection: "mysql://h9p1f0m1ufohw6c8:bvjuneizhu024ct8@td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/zuyd4r66g2sp77rz",
-  },
-};
+
