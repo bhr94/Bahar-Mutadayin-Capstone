6ce924719cba6626c81099fe0c470b32ddad74a7
bhr94 <bahar.mubtadayin@gmail.com>
2021-01-12 21:33:01 -0800

    added a connection into knexfile

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,16 +1,31 @@
-module.exports = {
-  client: "mysql",
-  // connection: {
-  //   host: "127.0.0.1",
-  //   user: "root",
-  //   password: "root",
-  //   database: "capstone",
-  //   charset: "utf8",
-  // },
-  production: {
-    host: "td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "h9p1f0m1ufohw6c8",
-    password: "bvjuneizhu024ct8",
-    connection: "mysql://h9p1f0m1ufohw6c8:bvjuneizhu024ct8@td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/zuyd4r66g2sp77rz",
-  },
-};
+// module.exports = {
+//   client: "mysql",
+//   // connection: {
+//   //   host: "127.0.0.1",
+//   //   user: "root",
+//   //   password: "root",
+//   //   database: "capstone",
+//   //   charset: "utf8",
+//   // },
+//   production: {
+//     host: "td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//     user: "h9p1f0m1ufohw6c8",
+//     password: "bvjuneizhu024ct8",
+//     connection: "mysql://h9p1f0m1ufohw6c8:bvjuneizhu024ct8@td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/zuyd4r66g2sp77rz",
+//   },
+// };
+let connectionInfo;
+
+if(process.env.JAWSDB_URL) {
+    connectionInfo = process.env.JAWSDB_URL;
+} else {
+    connectionInfo = {
+        host:process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password:process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    };
+}
+
+const connection = mysql.createConnection(connectionInfo);
