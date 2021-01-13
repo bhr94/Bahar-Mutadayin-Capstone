e56f10974790291869b900e67ea79dd32059faf4
bhr94 <bahar.mubtadayin@gmail.com>
2021-01-12 20:29:49 -0800

    added heroku db connection

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,16 +1,16 @@
 module.exports = {
   client: "mysql",
-  connection: {
-    host: "127.0.0.1",
-    user: "root",
-    password: "root",
-    database: "capstone",
-    charset: "utf8",
-  },
-  // production: {
-  //   host: "td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  //   user: "h9p1f0m1ufohw6c8",
-  //   password: "bvjuneizhu024ct8",
-  //   connection: process.env.JAWSDB_URL,
+  // connection: {
+  //   host: "127.0.0.1",
+  //   user: "root",
+  //   password: "root",
+  //   database: "capstone",
+  //   charset: "utf8",
   // },
+  production: {
+    host: "td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "h9p1f0m1ufohw6c8",
+    password: "bvjuneizhu024ct8",
+    connection: process.env.JAWSDB_URL,
+  },
 };
