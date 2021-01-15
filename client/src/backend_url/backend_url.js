af8a441d356604431da7c03b9d7d6c7c2dc13722
bhr94 <bahar.mubtadayin@gmail.com>
2021-01-14 20:33:15 -0800

    resolved the backend url issue

--- client/src/backend_url/backend_url.js
+++ client/src/backend_url/backend_url.js
@@ -1,3 +1,5 @@
-const backend_url = "http://localhost:8080";
-// const backend_url = "https://evening-gorge-97906.herokuapp.com"
+const backend_url =
+  process.env.NODE_ENV === "production"
+    ? "https://evening-gorge-97906.herokuapp.com"
+    : "http://localhost:8080";
 export default backend_url;
